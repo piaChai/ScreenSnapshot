79611df67915c95f976a7aafe1bfb0dbcf21990e
hulingzhi <hulingzhi@cvte.cn>
2014-06-16 17:36:50 +0800

    add x264 library

---
+++ ScreenSnapshot/x264Manager.h
@@ -0,0 +1,14 @@
+//
+//  x264Manager.h
+//  ScreenSnapshot
+//
+//  Created by hulingzhi on 14-6-16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "x264.h"
+
+@interface x264Manager : NSObject
+
+@end
---
+++ ScreenSnapshot/x264Manager.m
@@ -0,0 +1,13 @@
+//
+//  x264Manager.m
+//  ScreenSnapshot
+//
+//  Created by hulingzhi on 14-6-16.
+//
+//
+
+#import "x264Manager.h"
+
+@implementation x264Manager
+
+@end
